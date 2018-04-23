f9c0907cc91ea32b3307352305f5675e380fe29f
TheIllusiveC4 <itwasmec4@gmail.com>
2018-04-23 02:18:26 -0400

    version bump to 0.13

--- src/main/java/c4/conarm/ConstructsArmory.java
+++ src/main/java/c4/conarm/ConstructsArmory.java
@@ -22,7 +22,7 @@ public class ConstructsArmory {
 
     public static final String MODID = "conarm";
     public static final String MODNAME = "Construct's Armory";
-    public static final String MODVER = "0.0.12-a";
+    public static final String MODVER = "0.0.13-a";
 
     @SidedProxy(clientSide = "c4.conarm.proxy.ClientProxy", serverSide = "c4.conarm.proxy.CommonProxy")
     public static CommonProxy proxy;
