e368b2fb4f50f30b543fb36352c8496db48014e1
TheIllusiveC4 <itwasmec4@gmail.com>
2019-11-02 19:54:36 -0700

    change Ambitious trait behavior to avoid potential exploits, closes #172

--- src/main/java/c4/conarm/common/armor/traits/TraitAmbitious.java
+++ src/main/java/c4/conarm/common/armor/traits/TraitAmbitious.java
@@ -15,27 +15,33 @@ package c4.conarm.common.armor.traits;
 
 import c4.conarm.common.armor.utils.ArmorHelper;
 import c4.conarm.lib.traits.AbstractArmorTrait;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
+import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TraitAmbitious extends AbstractArmorTrait {
 
     public TraitAmbitious() {
         super("ambitious", 0xffffff);
-
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void onXPPickUp(PlayerPickupXpEvent evt) {
-        int xpValue = evt.getOrb().getXpValue();
-        if (xpValue > 0) {
-            int level = (int) ArmorHelper.getArmorAbilityLevel(evt.getEntityPlayer(), this.identifier);
-            int addXP = random.nextInt(level + 1);
-            if (addXP > 0) {
-                evt.getEntityPlayer().addExperience(addXP);
-            }
-        }
+    public void onLivingXPDrop(LivingExperienceDropEvent evt) {
+        int addXP = getXpToAdd(evt.getAttackingPlayer());
+        evt.setDroppedExperience(addXP + evt.getDroppedExperience());
+    }
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent evt) {
+        int addXP = getXpToAdd(evt.getPlayer());
+        evt.setExpToDrop(evt.getExpToDrop() + addXP);
+    }
+
+    private static int getXpToAdd(EntityPlayer player) {
+        int level = (int) ArmorHelper.getArmorAbilityLevel(player, ArmorTraits.ambitious.identifier);
+        return random.nextInt(level * 2 + 1) + 2;
     }
 }
