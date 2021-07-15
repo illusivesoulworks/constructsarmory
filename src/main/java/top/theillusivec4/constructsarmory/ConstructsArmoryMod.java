97661f052d179cf1e5d36f3156883d1dc3f5b471
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-07-15 01:26:29 -0700

    begin 1.16 port

---
+++ src/main/java/top/theillusivec4/constructsarmory/ConstructsArmoryMod.java
@@ -0,0 +1,12 @@
+package top.theillusivec4.constructsarmory;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ConstructsArmoryMod.MOD_ID)
+public class ConstructsArmoryMod {
+
+    public static final String MOD_ID = "constructsarmory";
+    public static final Logger LOGGER = LogManager.getLogger();
+}
