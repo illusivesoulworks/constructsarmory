e4914a337b0205279a18582fdc2f18e7687a02a2
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-06-19 04:44:46 -0700

    fix stats

--- src/main/java/top/theillusivec4/constructsarmory/common/stat/ArmorStatsBuilder.java
+++ src/main/java/top/theillusivec4/constructsarmory/common/stat/ArmorStatsBuilder.java
@@ -1,9 +1,7 @@
 package top.theillusivec4.constructsarmory.common.stat;
 
 import com.google.common.annotations.VisibleForTesting;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Nonnull;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,15 +15,12 @@ import slimeknights.tconstruct.library.tools.stat.ToolStats;
 import slimeknights.tconstruct.library.tools.stat.ToolStatsBuilder;
 import slimeknights.tconstruct.tools.item.ArmorSlotType;
 import top.theillusivec4.constructsarmory.api.ArmorMaterialStatsIdentifiers;
+import top.theillusivec4.constructsarmory.api.ArmorStatsCalculator;
 import top.theillusivec4.constructsarmory.common.stat.impl.MailMaterialStats;
 import top.theillusivec4.constructsarmory.common.stat.impl.PlateMaterialStats;
 
 @Getter(AccessLevel.PROTECTED)
 public final class ArmorStatsBuilder extends ToolStatsBuilder {
-
-  private static final int[] ARMOR_ARRAY =
-      new int[] {0, 1, 2, 3, 2, 3, 3, 0, 1, 2, 3, 2, 3, 0, 1, 2, 3, 2, 3, 3};
-  private static final Map<Float, float[]> ARMOR_VALUES = new HashMap<>();
 
   private final ArmorSlotType slotType;
   private final List<PlateMaterialStats> plates;
@@ -74,65 +69,17 @@ public final class ArmorStatsBuilder extends ToolStatsBuilder {
         getAverageValue(this.plates, PlateMaterialStats::getDurability) +
             this.toolData.getBonus(ToolStats.DURABILITY);
     double averageMailModifier = getAverageValue(this.mail, MailMaterialStats::getDurability, 1);
-
-    switch (this.slotType) {
-      case HELMET:
-        averagePlateDurability *= 0.6875f;
-        break;
-      case BOOTS:
-        averagePlateDurability *= 0.8125f;
-        break;
-      case LEGGINGS:
-        averagePlateDurability *= 0.9375f;
-    }
-    return Math.max(1, (int) (averagePlateDurability * averageMailModifier));
+    return Math.max(1,
+        (int) (ArmorStatsCalculator.getDurabilityStat((int) averagePlateDurability, this.slotType) *
+            averageMailModifier));
   }
 
   public float buildArmor() {
     double averageArmor = getAverageValue(this.plates, PlateMaterialStats::getArmor) +
         this.toolData.getBonus(ToolStats.ARMOR);
     double averageMailModifier = getAverageValue(this.mail, MailMaterialStats::getArmor, 0);
-    return getArmor(
-        Math.round((float) Math.max(0, averageArmor * averageMailModifier) * 100f) / 100f,
-        this.slotType);
-  }
-
-  public static float getArmor(float armor, ArmorSlotType slotType) {
-    final float finalArmor = armor;
-    float[] values = ARMOR_VALUES.computeIfAbsent(finalArmor, (k) -> {
-      float points = finalArmor;
-      float[] result = new float[] {0.0f, 0.0f, 0.0f, 0.0f};
-      float step;
-
-      if (k > 4.0f) {
-        step = 1.0f;
-      } else if (k > 2.0f) {
-        step = 0.5f;
-      } else {
-        step = 0.1f;
-      }
-
-      while (points >= step) {
-
-        for (int i = 0; i < ARMOR_ARRAY.length && points >= step; i++) {
-          int index = ARMOR_ARRAY[i];
-          result[index] += step;
-          points -= step;
-        }
-      }
-      result[0] += points;
-      return result;
-    });
-    int index = 0;
-
-    if (slotType == ArmorSlotType.HELMET) {
-      index = 1;
-    } else if (slotType == ArmorSlotType.LEGGINGS) {
-      index = 2;
-    } else if (slotType == ArmorSlotType.CHESTPLATE) {
-      index = 3;
-    }
-    return values[index];
+    return (float) (ArmorStatsCalculator.getArmorStat((int) averageArmor, this.slotType) *
+        averageMailModifier);
   }
 
   public float buildArmorToughness() {
--- src/main/java/top/theillusivec4/constructsarmory/common/stat/impl/PlateMaterialStats.java
+++ src/main/java/top/theillusivec4/constructsarmory/common/stat/impl/PlateMaterialStats.java
@@ -10,12 +10,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.Color;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import slimeknights.tconstruct.library.materials.stats.BaseMaterialStats;
 import slimeknights.tconstruct.library.materials.stats.IRepairableMaterialStats;
 import slimeknights.tconstruct.library.materials.stats.MaterialStatsId;
+import slimeknights.tconstruct.library.tools.stat.FloatToolStat;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
+import slimeknights.tconstruct.library.utils.Util;
 import top.theillusivec4.constructsarmory.ConstructsArmoryMod;
+import top.theillusivec4.constructsarmory.api.ArmorStatsCalculator;
 import top.theillusivec4.constructsarmory.common.stat.ConstructsArmoryStats;
 
 @Getter
@@ -69,12 +76,31 @@ public class PlateMaterialStats extends BaseMaterialStats implements IRepairable
   @Nonnull
   public List<ITextComponent> getLocalizedInfo() {
     List<ITextComponent> info = Lists.newArrayList();
-    info.add(ToolStats.DURABILITY.formatValue(this.durability));
-    info.add(ToolStats.ARMOR.formatValue(this.armor));
+    int[] durabilities = ArmorStatsCalculator.getDurabilityStats(this.durability);
+    info.add(formatArray(ToolStats.DURABILITY, durabilities[1], durabilities[3], durabilities[2],
+        durabilities[0]));
+    float[] armors = ArmorStatsCalculator.getArmorStats(this.armor);
+    info.add(formatArray(ToolStats.ARMOR, armors[1], armors[3], armors[2], armors[0]));
     info.add(ToolStats.ARMOR_TOUGHNESS.formatValue(this.toughness));
     info.add(ToolStats.KNOCKBACK_RESISTANCE.formatValue(this.knockbackResistance));
     info.add(ConstructsArmoryStats.MOVEMENT_SPEED.formatValue(this.movementSpeed));
     return info;
+  }
+
+  public ITextComponent formatArray(FloatToolStat toolStat, float num1, float num2, float num3,
+                                    float num4) {
+    String name = toolStat.getName().getPath();
+    Color color = toolStat.getColor();
+    String loc = "tool_stat." + ConstructsArmoryMod.MOD_ID + "." + name;
+    return new TranslationTextComponent(loc).appendSibling(
+        new StringTextComponent(Util.COMMA_FORMAT.format(num1) + "/").modifyStyle(
+            style -> style.setColor(color))).appendSibling(
+        new StringTextComponent(Util.COMMA_FORMAT.format(num2) + "/").modifyStyle(
+            style -> style.setColor(color))).appendSibling(
+        new StringTextComponent(Util.COMMA_FORMAT.format(num3) + "/").modifyStyle(
+            style -> style.setColor(color))).appendSibling(
+        new StringTextComponent(Util.COMMA_FORMAT.format(num4)).modifyStyle(
+            style -> style.setColor(color)));
   }
 
   @Override
